e90650d573b5f66a37adb8eced80b9035536d20d
Diptanshu Singh <diptanshu.singh@gmail.com>
2016-10-26 00:23:54 -0500

    prodconsumer

---
+++ prodcon.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+var comm = make(chan bool)
+var done = make(chan bool)
+
+func producer(){
+
+	for i := 0;i < 10 ; i++ {
+		comm <- true
+	}
+	done <- true
+}
+
+func consumer(){
+	for {
+		communication := <- comm
+		fmt.Println("Communication from Producer receiveed!",communication)
+	}
+}
+
+func main(){
+	go producer()
+	go consumer()
+	<- done
+	fmt.Println("All Done!")
+}
